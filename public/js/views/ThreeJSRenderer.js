9ce9487b59954adb90c0b5e03c421f9306971a3d
Lucas Doyle <lucas.p.doyle@gmail.com>
2014-09-05 13:14:46 +0900

    raycasting framework

--- public/js/views/ThreeJSRenderer.js
+++ public/js/views/ThreeJSRenderer.js
@@ -4,6 +4,10 @@ var ThreeJSRenderer = BaseRealtimeView.extend({
 
   template: '#renderer-template',
 
+  pointerVector: new THREE.Vector3(),
+  rayCaster: new THREE.Raycaster(),
+  projector: new THREE.Projector(),
+
   events: {
     'click #feed-toggle': function(e) {
       e.preventDefault();
@@ -15,19 +19,19 @@ var ThreeJSRenderer = BaseRealtimeView.extend({
     },
 
     'click #translate-mode': function(e) {
-      this.control.set('mode', 'translate');
+      this.transformControl.set('mode', 'translate');
       e.preventDefault();
       e.stopPropagation();
     },
 
     'click #rotate-mode': function(e) {
-      this.control.set('mode', 'rotate');
+      this.transformControl.set('mode', 'rotate');
       e.preventDefault();
       e.stopPropagation();
     },
 
     'click #scale-mode': function(e) {
-      this.control.set('mode', 'scale');
+      this.transformControl.set('mode', 'scale');
       e.preventDefault();
       e.stopPropagation();
     },
@@ -46,6 +50,66 @@ var ThreeJSRenderer = BaseRealtimeView.extend({
 
       e.preventDefault();
       e.stopPropagation();
+    },
+
+    'click #canvas-anchor > canvas': function(e) {
+
+      var rect = this.renderer.domElement.getBoundingClientRect();
+
+      var x = (e.clientX - rect.left) / rect.width;
+      var y = (e.clientY - rect.top) / rect.height;
+      this.pointerVector.set((x) * 2 - 1, - (y) * 2 + 1, 0.5);
+
+      this.projector.unprojectVector(this.pointerVector, this.camera);
+      this.rayCaster.set(this.camera.position, this.pointerVector.sub(this.camera.position).normalize());
+
+      var searchList = [];
+      var intersections = this.rayCaster.intersectObjects(this.collection.map(function(model) {
+        var mesh = model.getMesh();
+        searchList.push({
+          drawable: model,
+          mesh: mesh
+        });
+        return mesh;
+      }));
+
+      var raycastedDrawable
+      if (intersections[0]) {
+         raycastedDrawable = _.findWhere(searchList, {
+          mesh: intersections[0].object
+        }).drawable;
+      }
+      this.collection.trigger('raycast', raycastedDrawable)
+      raycastedDrawable.trigger('raycast', raycastedDrawable)
+
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  },
+
+  // TODO
+  onClick: function() {
+
+    var prevCamera = camera;
+
+    camera = new THREE.PerspectiveCamera();
+    camera.position.copy(prevCamera.position);
+    camera.rotation.copy(prevCamera.rotation);
+
+    var MODE = {
+      TRACKBALL: 0,
+      FLY: 1
+    };
+
+    switch (mode) {
+      case MODE.FLY:
+        controls = new THREE.TrackballControls(camera);
+        mode = MODE.TRACKBALL;
+        break;
+      case MODE.TRACKBALL:
+        controls = new THREE.FlyControls(camera);
+        mode = MODE.FLY;
+        break;
     }
   },
 
@@ -103,7 +167,7 @@ var ThreeJSRenderer = BaseRealtimeView.extend({
   collectionEvents: {
     'add': function(drawable) {
       this.addDrawable(drawable);
-      this.control.attachDrawable(drawable);
+      this.transformControl.attachDrawable(drawable);
       this._render();
     },
 
@@ -149,7 +213,7 @@ var ThreeJSRenderer = BaseRealtimeView.extend({
       this.setupCamera();
       this.setupScene();
 
-      this.control = new Control({
+      this.transformControl = new TransformControl({
         renderer: this
       });
 
